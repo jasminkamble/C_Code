58e229bcce9c815c08f7e7bb553787041faf6ed7
jasminkamble <jasminkamble2@gmail.com>
2026-03-06 14:52:38 +0000

    06/03/26

--- call_by_ref.c
+++ call_by_ref.c
@@ -1,13 +1,38 @@
-#include<stdio.h>  
-void change(int *num) {    
-    printf("Before adding value inside function num=%d \n",*num);    
-    (*num) += 100;    
-    printf("After adding value inside function num=%d \n", *num);    
-}      
-int main() {    
-    int x=100;    
-    printf("Before function call x=%d \n", x);    
-    change(&x);//passing reference in function    
-    printf("After function call x=%d \n", x);    
-return 0;  
-}    
+
+// #include<stdio.h>
+// void change(int *num)
+// {
+//     printf("\nbefore adding value in function num=%d",*num);
+//     *num+=100;
+//     printf("\nafter adding thevalue inside the function num=%d",*num);
+// }
+// int main()
+// {
+//     int x=400;
+//     printf("\nbefore calling the function x=%d",x);
+//     change(&x);
+//     printf("\nafter calling the function value x=%d\n",x);
+// }
+
+
+// Swap of two number
+
+#include<stdio.h>
+void swap(int *a,int *b)
+{
+    int result;
+    result=*a;
+    *a=*b;
+    *b=*a;
+}
+int main()
+{
+    int a,b,result;
+    printf("Enter the numbers a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%d",&b);
+    swap(&a,&b);
+    printf("after calling the swap function a=%d,b=%d\n",a,b);
+
+}
--- call_by_value.c
+++ call_by_value.c
@@ -1,16 +1,39 @@
-// call by value
+// addition
+
+// #include<stdio.h>
+// void change(int num)
+// {
+//     printf("Before adding value inside function num=%d\n",num);
+//     num+=100;
+//     printf("after adding value inside the function num=%d\n",num);
+// }
+// int main()
+// {
+//     int x=100;
+//     printf("befor caling function x=%d\n",x);
+//     change(x);
+//     printf("after calling the value  x=%d\n",x);
+// }
+
+// swapping
+
 
 #include<stdio.h>
-void change(int num)
+void swap(int a,int b)
 {
-    printf("Before adding value inside function num=%d\n",num);
-    num+=100;
-    printf("after adding value inside the function num=%d\n",num);
+    int result;
+    result=a;
+    a=b;
+    b=a;
 }
 int main()
 {
-    int x=100;
-    printf("befor caling function x=%d\n",x);
-    change(x);
-    printf("after calling the value  x=%d\n",x);
+    int a,b,result;
+    printf("Enter the numbers a:");
+    scanf("%d",&a);
+    printf("enter the number b:");
+    scanf("%d",&b);
+    swap(a,b);
+    printf("after calling the swap function a=%d,b=%d",a,b);
+
 }
