357e07d41d4dcecfb41fb98b7b2bcd215abc06c4
jasminkamble <jasminkamble2@gmail.com>
2026-04-01 14:38:56 +0000

    01/04

--- recursion/prime_num_or_not.c
+++ recursion/prime_num_or_not.c
@@ -12,8 +12,10 @@ int prime(int a)
         for(i=2;i<=a/2;i++){
             if(a%i==1)
             {
-                
+                prime 1;
             }
+            else
         }
+        if
     }
 }
